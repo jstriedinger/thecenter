3efdd262e45bb70fb5beef382522d5e6426b73ee
Jose Striedinger <jstriedinger2090@gmail.com>
2019-03-04 20:01:41 -0500

    init

---
+++ src/js/app.js
@@ -0,0 +1,58 @@
+import "jquery";
+import "../sass/main.scss";
+import TweenMax from "gsap/TweenMax"; // imports TweenLite
+import TimelineMax from "gsap/TimelineMax"; // imports TimelineLite
+import scrollTo from "gsap/ScrollToPlugin";
+export var fadeSpeed = 0.4
+
+
+console.log("yeah!");
+
+function toggleModal(id,open = true)
+{
+    if(open)
+    {
+        $(".modal#"+id).addClass("is-active");
+        TweenMax.to("#"+id, fadeSpeed, {opacity: 1});
+    }
+    else
+    {
+        TweenMax.to($(".modal#"+id), fadeSpeed, {opacity: 0, onComplete: function(){
+            TweenMax.delayedCall(0.5, function(){$(".modal#"+id).removeClass("is-active")});
+            console.log("stop video")
+
+			$(".modal#videomodal iframe").each(function() { 
+			        var src= $(this).attr('src');
+			        $(this).attr('src',src);  
+			});
+
+            
+        }})
+    }
+}
+
+
+
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  toggleModal("videomodal");
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+  $("#playvideo").click(function(event ){
+  	event.preventDefault();
+  	toggleModal("videomodal");
+  })
+
+  $(".modal .close-button").click(function(event ){
+        event.preventDefault();
+        console.log("closing")
+        let modal = $(this).data("modal");
+        toggleModal("videomodal",false)
+    })
+});
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+
+
+const config = {
+  context: path.resolve(__dirname, "src"),
+  // configurations here
+  entry: {
+    app: './js/app.js'
+  },
+  output: {
+    filename: './js/[name].js',
+    // Output path using nodeJs path module
+    path: path.resolve(__dirname, 'dist')
+  },
+  // Adding jQuery as external library
+  externals: {
+    jquery: 'jQuery'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { url: false, sourceMap: true } }, 
+          { loader: 'postcss-loader', options: { sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+              loader: 'babel-loader',
+              options: {
+                  presets: ['env']
+              }   
+          }
+        ]
+      }
+    ]
+  },
+  watch: true,
+  plugins: [
+    
+    require('autoprefixer'),
+    new MiniCssExtractPlugin({filename: "/css/[name].css"}),
+    new BrowserSyncPlugin({
+        files: [
+          './**/*.php',
+          './**/*.twig'
+        ],
+        host: 'localhost',
+        port: 3000,
+        proxy: 'http://localhost/center',
+        logPrefix: 'webpack',
+        logLevel: 'debug',
+        ghostMode: false
+    })
+  ]
+};
+
+module.exports = config;
